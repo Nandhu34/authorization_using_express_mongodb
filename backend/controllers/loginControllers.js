1a8f6463aae0de09801a1cefe7c5c219e478cb43
nandhakumar <nandhakumarselva2000@gmail.com>
2024-07-19 09:54:44 +0530

    sample controllers for login has been defined

--- backend/controllers/loginControllers.js
+++ backend/controllers/loginControllers.js
@@ -5,4 +5,33 @@ function RegisterNewUser(req,res)
 
 
 
-function loginUser(req,res,next)
+function loginUser(req,res)
+{
+    console.log(" login user ")
+}
+
+function logoutUser(req,res)
+{
+    console.log(" logout user ")
+
+}
+
+
+function deleteAccount(req,res)
+{
+    console.log(" delete account permanently ")
+}
+
+function updateUser(req,res)
+{
+    console.log(" update user  details ")
+}
+
+function forgetPassword(req,res)
+{
+    console.log("forget password ")
+}
+
+
+
+module.exports = {RegisterNewUser,loginUser,logoutUser,deleteAccount,updateUser,forgetPassword}
