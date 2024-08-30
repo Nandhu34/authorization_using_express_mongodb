2ed59a0163edb0aa6bf77f9168a9afa7b46a518c
nandhakumar <nandhakumarselva2000@gmail.com>
2024-08-30 19:30:31 +0530

    working on login part frontend

--- frontend/frontend_app/src/components/loginRegister/loginComponent.jsx
+++ frontend/frontend_app/src/components/loginRegister/loginComponent.jsx
@@ -10,7 +10,46 @@ function  LogIn() {
     console.log(" login ")
    const [userName,setuseName] = useState('')
    const [password,setPassword] = useState('')
+   const [passwordError ,setPasswordError] = useState('')
 
+
+
+
+   const validatePassword = (password) => {
+
+    const minLength = 10;
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+    // const hasThreeNumbers = (password.match(/\d/g) || []).length >= 3;
+    const lengthValid = password.length >= minLength;
+
+    if (!lengthValid) {
+        console.log("Password must be at least 10 characters long.")
+        return "Password must be at least 10 characters long.";
+    }
+    else{
+      setPasswordError('')
+    }
+    if (!hasUppercase) {
+        console.log("Password must contain at least one uppercase letter.")
+        return "Password must contain at least one uppercase letter.";
+    }
+    else{
+      setPasswordError('')
+    }
+    if (!hasSpecialChar) {
+        console.log("Password must contain at least one special character.")
+        return "Password must contain at least one special character.";
+    }
+    else{
+      setPasswordError('')
+    }
+    // if (!hasThreeNumbers) {
+    //     console.log("Password must contain at least three numbers.")
+    //     return "Password must contain at least three numbers.";
+    // }
+    return ""; // No errors
+};
 return (
   <>
   <div className="register-main-div">
@@ -26,15 +65,34 @@ return (
           <div className ="inner-box-of-side-login-content">
 
              <div className ="username-div">
-                  <input type="text" className ="username-field" placeholder="Enter user email" onChange={(e)=>{console.log(userName);setuseName(e.target.value)}}/>
+                  <input type="text" className ="username-field" placeholder="Enter user email"  value ={userName}onChange={(e)=>{console.log(userName);setuseName(e.target.value)}}/>
              </div>
              <div className ="password-div">
 
-                <input type="password"  className ="password-field" placeholder="Enter Your Password" onChange={(e)=>{console.log(password);setPassword(e.target.value)}}/>
-
+                <input type="password"  className ="password-field" placeholder="Enter Your Password"  value = {password}onChange={(e)=>{console.log(password);setPassword(e.target.value); let  validateError = validatePassword; setPasswordError(validateError)}}/>
+                {passwordError.length>1 && passwordError }
              </div>
-              <div className="" value = "submit-button" onClick={()=>{setPassword('')}}>
-                <input type ="submit"  value = "submit" onClick = {handleSubmit } />
+              <div>
+              <input
+                                type="submit"
+                                value="submit"
+                                onClick={() => {
+
+                                  if (passwordError) 
+                                    {
+                                      console.log(" validation error in password ")
+                                    }
+                                    else 
+                                    {
+                                    handleSubmit(userName, password);
+
+                                    setuseName(''); // Reset userName
+                                    setPassword(''); // Reset password
+                                    console.log(" reseted ")
+                                    }
+
+                                }}
+                            />
              </div>
             
           </div>
@@ -50,11 +108,11 @@ return (
 
 
 
-  function handleSubmit()
+  function handleSubmit(username ,password )
   {
     
-    console.log("handle submit function ")
-    return (<><p>hello </p></>)
+    console.log("handle submit function "+ username,password)
+    
   }
   
   export default LogIn; 
